f0a32dfcae2248f7967dfc879c7d3828f9b9ac5d
Daniel Cruz <adm.dcf@gmail.com>
2021-05-29 15:44:21 -0300

    Article Model Added

--- blog_app/blog/models.py
+++ blog_app/blog/models.py
@@ -31,3 +31,55 @@ class Article(BaseModel):
         con.close()
 
         return article
+
+    @classmethod
+    def get_by_title(cls, title: str):
+        con = sqlite3.connect(os.getenv("DATABASE_NAME", "database.db"))
+        con.row_factory = sqlite3.Row
+
+        cur = con.cursor()
+        cur.execute("SELECT * FROM articles WHERE title = ?", (title,))
+
+        record = cur.fetchone()
+
+        if record is None:
+            raise NotFound
+
+        article = cls(**record)  # Row can be unpacked as dict
+        con.close()
+
+        return article
+
+    @classmethod
+    def list(cls) -> List["Article"]:
+        con = sqlite3.connect(os.getenv("DATABASE_NAME", "database.db"))
+        con.row_factory = sqlite3.Row
+
+        cur = con.cursor()
+        cur.execute("SELECT * FROM articles")
+
+        records = cur.fetchall()
+        articles = [cls(**record) for record in records]
+        con.close()
+
+        return articles
+
+    def save(self) -> "Article":
+        with sqlite3.connect(os.getenv("DATABASE_NAME", "database.db")) as con:
+            cur = con.cursor()
+            cur.execute(
+                "INSERT INTO articles (id,author,title,content) VALUES(?, ?, ?, ?)",
+                (self.id, self.author, self.title, self.content),
+            )
+            con.commit()
+
+        return self
+
+    @classmethod
+    def create_table(cls, database_name="database.db"):
+        conn = sqlite3.connect(database_name)
+
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS articles (id TEXT, author TEXT, title TEXT, content TEXT)"
+        )
+        conn.close()
